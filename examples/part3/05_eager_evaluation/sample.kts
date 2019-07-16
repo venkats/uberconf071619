feb174b1428ab82a39f7d8cb2f1dca1a0c5f1f30
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 14:56:48 -0600

    eager evaluation

---
+++ examples/part3/05_eager_evaluation/sample.kts
@@ -0,0 +1,18 @@
+val numbers = listOf(1, 2, 3, 5, 4, 6, 7, 8, 9, 10)
+
+fun isEven(n: Int): Boolean {
+  println("isEven called for $n")
+  return n % 2 == 0
+}
+
+fun isGT(n: Int): Boolean {
+  return n > 3
+}
+
+fun doubleIt(n: Int) = n * 2
+
+println(
+  numbers.filter(::isEven)
+    .filter(::isGT)
+    .map(::doubleIt)
+    .first())

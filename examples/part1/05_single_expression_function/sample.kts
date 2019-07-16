20255121a418c18b8d90a0c057a44087e9997ec6
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 10:00:55 -0600

    single expression function

---
+++ examples/part1/05_single_expression_function/sample.kts
@@ -0,0 +1,10 @@
+fun greet() = when {
+  else -> "Hello, this is stupid"
+}
+
+println(greet())
+
+//a stupid example of a single expression function
+
+//we don't have to specify the return type for 
+//single expression functions

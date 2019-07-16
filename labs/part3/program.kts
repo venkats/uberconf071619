be96256bf8893db1f332373eeb3c20cd30dd1c7b
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 14:58:41 -0600

    post lab

---
+++ labs/part3/program.kts
@@ -0,0 +1,46 @@
+class SquareMatrix(val values: List<List<Int>>) {
+  override fun toString(): String =
+    values.map { row -> row.joinToString(" ") }.joinToString("\n")
+    
+  operator fun times(other: SquareMatrix): SquareMatrix {
+    return this //need to change this
+  }
+  
+  init {
+    if(values.size != values[0].size) throw RuntimeException("Matrix should be square")
+  }
+}
+
+val matrix1 = SquareMatrix(listOf(
+  listOf(1, 2, 3),
+  listOf(4, 5, 6),
+  listOf(7, 8, 9)))
+
+val matrix2 = SquareMatrix(listOf(
+  listOf(1, 1, 1),
+  listOf(2, 2, 2),
+  listOf(3, 3, 3)))
+               
+println("Matrix 1:")
+println(matrix1)
+
+println("Matrix 2:")
+println(matrix2)
+
+println("Product:")
+println(matrix1 * matrix2)
+
+/*
+Matrix 1:
+1 2 3
+4 5 6
+7 8 9
+Matrix 2:
+1 1 1
+2 2 2
+3 3 3
+Product:
+14 14 14
+32 32 32
+50 50 50
+*/

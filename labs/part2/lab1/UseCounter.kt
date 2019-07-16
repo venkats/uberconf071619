d34ad393b1f348e1d2525163823b8e3e71247c0a
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 11:59:49 -0600

    post lab

---
+++ labs/part2/lab1/UseCounter.kt
@@ -0,0 +1,33 @@
+package com.agiledeveloper
+
+//Uncomment one part at a time, write minimum code to make that part to work. Then
+//uncomment the next part to move forward.
+
+fun main() {
+//  var counter = Counter(3)
+  
+//  println(counter.value) //3
+
+//  for(i in 1..3) {
+//    counter++
+//  }
+
+//  println(counter.value) //6
+
+//  for(i in 1..5) {
+//    counter--
+//  }
+
+//  println(counter.value) //1
+
+//  counter--
+//  counter--
+
+//  println(counter.value) //0
+
+//  try {
+//    Counter(-1)
+//  } catch(ex: RuntimeException) {
+//    println(ex) //java.lang.RuntimeException: Invalid value
+//  }
+}

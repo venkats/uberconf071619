60e0addcac86debfd2c089d41fc4f199d13e27b6
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 16:05:35 -0600

    receivers

---
+++ examples/part4/05_receivers/sample.kts
@@ -0,0 +1,10 @@
+//val greet : (String) -> String = { name -> "Hello $name" }
+//
+//println(greet("Jane"))
+
+val greet : String.(String, Int) -> String = 
+  { name, age -> "${this.toUpperCase()} $name you are $age today" }
+
+//println(greet("Hello", "Jane", 2)) //this works
+println("Hello".greet("Jane", 2))     
+println("howdy".greet("Jane", 4))

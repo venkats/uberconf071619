50aa214bb837d96df53b47e7db7bec7c000bfe52
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 10:25:59 -0600

    safe navigation

---
+++ examples/part1/12_safe_navigation/sample.kts
@@ -0,0 +1,15 @@
+fun nickName(name: String): String? {
+  if(name == "Robert")
+    return "Bob"
+  
+  return null
+}
+           
+fun use(name: String) {
+  val result = nickName(name)
+  val len = result?.length //len is of type Int?
+  println("For $name, nick name is $result of length ${len}")
+}
+
+use("Robert")
+use("Venkat")

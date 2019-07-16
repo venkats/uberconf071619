371b1360e9e03527fe8a5a58527575742094f9ca
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 09:48:33 -0600

    using val

---
+++ examples/part1/01_using_val/sample.kts
@@ -0,0 +1,11 @@
+val greet: String = "hello"
+
+println(greet)
+
+//constant
+
+//greet = "hello" //ERROR
+
+//val in Kotlin is the same as final in Java for variables
+//val protects only the variable or the reference and not
+//the content referred to.

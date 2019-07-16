291979b31889acfe91b6c70eda2d5a50833c4e3f
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 10:04:59 -0600

    multiline functions

---
+++ examples/part1/07_multiline_functions/sample.kts
@@ -0,0 +1,12 @@
+fun greet(): String {
+  val message = "Hello there"
+  return message
+}
+
+println(greet())
+
+//multi-line (block) functions
+//return type is required or considere to be Unit (void)
+//otherwise
+
+//return keyword is required for anything other than Unit

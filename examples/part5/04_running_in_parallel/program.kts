71c8bafc847f92fb2033ef1ea6e88d268f5846e3
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 17:06:51 -0600

    running in parallel

---
+++ examples/part5/04_running_in_parallel/program.kts
@@ -0,0 +1,21 @@
+import kotlinx.coroutines.*
+
+suspend fun task1() {
+  println("entering task1 in ${Thread.currentThread()}")
+  yield()
+  println("exiting task1 in ${Thread.currentThread()}")
+}
+
+suspend fun task2() {
+  println("entering task2 in ${Thread.currentThread()}")
+  yield()
+  println("exiting task2 in ${Thread.currentThread()}")
+}
+
+runBlocking {
+  withContext(Dispatchers.IO) {
+    launch { task1() }
+    launch { task2() }
+  }
+  println("called the two tasks")
+}

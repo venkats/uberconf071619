acb0c7b6ec3b22c62964b46102f00012fde38f9b
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-16 09:53:29 -0600

    type inference

---
+++ examples/part1/02_type_inference/sample.kts
@@ -0,0 +1,9 @@
+//val greet: String = "hello"
+val greet = "hello"
+//greet is inferred at compile time to be of type String
+
+println(greet)
+
+//use type inference for variables
+//for fields and also for (limited extent) function
+//return types
